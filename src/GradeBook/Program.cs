b02ae8ac4a7db2f171d384f1ec381fce900569da
Gabriel Tomio Nardes <gabrieltomio@protonmail.com>
2020-03-13 09:08:37 -0300

    comments removed

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,14 +5,10 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            // var bookName = BookRender.RenderBookName();
-            // var book = BookRender.RenderAddGrades(bookName);
-            // BookRender.RenderStatistics(book);
-            // BookRender.RenderLog();
-            InMemoryBook book = null; // new DiskBook("teste");
-            book.AddGrade(20.3);
-            book.AddGrade(80.1);
-            book.AddGrade(23.1);
+            var bookName = BookRender.RenderBookName();
+            var book = BookRender.RenderAddGrades(bookName);
+            BookRender.RenderStatistics(book);
+            BookRender.RenderLog();
         }
     }
 }
